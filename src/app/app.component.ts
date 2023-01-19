5a8b3b9bbec636759ef5e09c9a7572429d1e5cf6
Damian Tarnawsky <damian@ionic.io>
2023-01-19 11:13:22 -0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import ScanbotSdk, {
+  CameraImageFormat,
+  DocumentScannerConfiguration,
+  FileEncryptionMode,
+  ScanbotSDKConfiguration
+} from 'cordova-plugin-scanbot-sdk';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  public SDK = (ScanbotSdk as any).promisify();
+  constructor() {
+    this.initScanbotSdk();
+   }
+
+  private async initScanbotSdk(): Promise<any> {
+    const config: ScanbotSDKConfiguration = {
+      loggingEnabled: true,
+      licenseKey: 'undefined',
+      documentDetectorMode: 'ML_BASED',
+      useCameraX: true,
+      allowXnnpackAcceleration: false,
+      allowGpuAcceleration: false,
+    };
+
+
+    return await this.SDK.initializeSdk(config);
+  }
 }
